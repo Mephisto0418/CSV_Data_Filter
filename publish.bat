5e0e0c46c374aa98ff9d2c7921c8eb4339f1d1e3
Mephisto0418 <boris850418@gmail.com>
2025-06-05 01:17:07 +0800

    更新README.md中的版本日期，將2024年改為2025年

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
 cd CSV_Data_Filter
-dotnet publish -c public -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=false
+dotnet publish -c public -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=false /p:PublishReadyToRun=true /p:IncludeNativeLibrariesForSelfExtract=true
